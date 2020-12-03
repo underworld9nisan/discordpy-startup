33da4cf01faa5ce4aef107a45bff7760d6400c57
underworld9nisan <75453221+underworld9nisan@users.noreply.github.com>
2020-12-04 07:59:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,66 +1,68 @@
 import discord
+from discord.ext import commands
+import asyncio
 
-client = discord.Client()
-
+client = commands.Bot(command_prefix='.')
 @client.event
-async def on_message(message):
-    """メンバー募集 (.rect@数字)"""
-    if message.content.startswith(".rect"):
-        mcount = int(message.content[6:len(message.content)])
-        text= "あと{}人 募集中\n"
-        revmsg = text.format(mcount)
-        #friend_list 押した人のList
-        frelist = []
-        msg = await client.send_message(message.channel, revmsg)
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
-        #投票の欄
-        await client.add_reaction(msg, '\u21a9')
-        await client.add_reaction(msg, '⏫')
-        await client.pin_message(msg)
+@client.command()
+async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
+    cnt, settime = int(cnt), float(settime)
+    reaction_member = [">>>"]
+    test = discord.Embed(title=about,colour=0x1e90ff)
+    test.add_field(name=f"あと{cnt}人 募集中\n", value=None, inline=True)
+    msg = await ctx.send(embed=test)
+    #投票の欄
+    await msg.add_reaction('⏫')
+    await msg.add_reaction('✖')
 
-        #リアクションをチェックする
-        while len(frelist) < int(message.content[6:len(message.content)]):
-            target_reaction = await client.wait_for_reaction(message=msg)
-            #発言したユーザが同一でない場合 真
-            if target_reaction.user != msg.author:
-                #==============================================================
-                #押された絵文字が既存のものの場合 >> 左　del
-                if target_reaction.reaction.emoji == '\u21a9':
-                    #==========================================================
-                    #◀のリアクションに追加があったら反応 frelistにuser.nameがあった場合　真
-                    if target_reaction.user.name in frelist:
-                        frelist.remove(target_reaction.user.name)
-                        mcount += 1
-                        #リストから名前削除
-                        await client.edit_message(msg, text.format(mcount) +
-                                                        '\n'.join(frelist))
-                            #メッセージを書き換え
-
-                    else:
-                        pass
-                #==============================================================
-                #押された絵文字が既存のものの場合　>> 右　add
-                elif target_reaction.reaction.emoji == '⏫':
-                    if target_reaction.user.name in frelist:
-                        pass
-
-                    else:
-                        frelist.append(target_reaction.user.name)
-                        #リストに名前追加
-                        mcount = mcount - 1
-                        await client.edit_message(msg, text.format(mcount) +
-                                                        '\n'.join(frelist))
-
-
-                elif target_reaction.reaction.emoji == '✖':
-                        await client.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
-                        await client.unpin_message(msg)
-                        break
-                await client.remove_reaction(msg, target_reaction.reaction.emoji, target_reaction.user)
-                #ユーザーがつけたリアクションを消す※権限によってはエラー
-                #==============================================================
+    def check(reaction, user):
+        emoji = str(reaction.emoji)
+        if user.bot == True:    # botは無視
+            pass
         else:
-            await client.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
+            return emoji == '⏫' or emoji == '✖'
+
+    while len(reaction_member)-1 <= cnt:
+        try:
+            reaction, user = await client.wait_for('reaction_add', timeout=settime, check=check)
+        except asyncio.TimeoutError:
+            await ctx.send('残念、人が足りなかったようだ...')
+            break
+        else:
+            print(str(reaction.emoji))
+            if str(reaction.emoji) == '⏫':
+                reaction_member.append(user.name)
+                cnt -= 1
+                test = discord.Embed(title=about,colour=0x1e90ff)
+                test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
+                await msg.edit(embed=test)
+
+                if cnt == 0:
+                    test = discord.Embed(title=about,colour=0x1e90ff)
+                    test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
+                    await msg.edit(embed=test)
+                    finish = discord.Embed(title=about,colour=0x1e90ff)
+                    finish.add_field(name="おっと、メンバーがきまったようだ",value='\n'.join(reaction_member), inline=True)
+                    await ctx.send(embed=finish)
+
+            elif str(reaction.emoji) == '✖':
+                if user.name in reaction_member:
+                    reaction_member.remove(user.name)
+                    cnt += 1
+                    test = discord.Embed(title=about,colour=0x1e90ff)
+                    test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
+                    await msg.edit(embed=test)
+                else:
+                    pass
+        # リアクション消す。メッセージ管理権限がないとForbidden:エラーが出ます。
+        await msg.remove_reaction(str(reaction.emoji), user)
+
 
 
 bot.run(token)
