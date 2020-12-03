3d2c121eeff0e153f6fce4022c74f96ef685d97a
underworld9nisan <75453221+underworld9nisan@users.noreply.github.com>
2020-12-04 07:57:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,66 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    """メンバー募集 (.rect@数字)"""
+    if message.content.startswith(".rect"):
+        mcount = int(message.content[6:len(message.content)])
+        text= "あと{}人 募集中\n"
+        revmsg = text.format(mcount)
+        #friend_list 押した人のList
+        frelist = []
+        msg = await client.send_message(message.channel, revmsg)
+
+        #投票の欄
+        await client.add_reaction(msg, '\u21a9')
+        await client.add_reaction(msg, '⏫')
+        await client.pin_message(msg)
+
+        #リアクションをチェックする
+        while len(frelist) < int(message.content[6:len(message.content)]):
+            target_reaction = await client.wait_for_reaction(message=msg)
+            #発言したユーザが同一でない場合 真
+            if target_reaction.user != msg.author:
+                #==============================================================
+                #押された絵文字が既存のものの場合 >> 左　del
+                if target_reaction.reaction.emoji == '\u21a9':
+                    #==========================================================
+                    #◀のリアクションに追加があったら反応 frelistにuser.nameがあった場合　真
+                    if target_reaction.user.name in frelist:
+                        frelist.remove(target_reaction.user.name)
+                        mcount += 1
+                        #リストから名前削除
+                        await client.edit_message(msg, text.format(mcount) +
+                                                        '\n'.join(frelist))
+                            #メッセージを書き換え
+
+                    else:
+                        pass
+                #==============================================================
+                #押された絵文字が既存のものの場合　>> 右　add
+                elif target_reaction.reaction.emoji == '⏫':
+                    if target_reaction.user.name in frelist:
+                        pass
+
+                    else:
+                        frelist.append(target_reaction.user.name)
+                        #リストに名前追加
+                        mcount = mcount - 1
+                        await client.edit_message(msg, text.format(mcount) +
+                                                        '\n'.join(frelist))
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+                elif target_reaction.reaction.emoji == '✖':
+                        await client.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
+                        await client.unpin_message(msg)
+                        break
+                await client.remove_reaction(msg, target_reaction.reaction.emoji, target_reaction.user)
+                #ユーザーがつけたリアクションを消す※権限によってはエラー
+                #==============================================================
+        else:
+            await client.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
 
 
 bot.run(token)
